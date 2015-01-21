b3f579175f56428d779dac70c26c5aaa4c495635
lecion <ylc931116@gmail.com>
2015-01-21 23:34:33 +0800

    完善URL处理

--- src/com/yliec/Gather.java
+++ src/com/yliec/Gather.java
@@ -16,14 +16,13 @@ import java.util.regex.Pattern;
  * Created by Lecion on 1/21/15.
  */
 public class Gather {
-
     public void getPage(String urlStr) throws MalformedURLException {
         URL url = new URL(urlStr);
         try {
             URLConnection conn = url.openConnection();
             InputStream is = conn.getInputStream();
             String content = readContentFromStream(is);
-            getUrlFromContent(content);
+            getUrlFromContent(content, urlStr);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -42,9 +41,19 @@ public class Gather {
     /**
      * 从页面内容获取URL列表，正则表达式处理
      * @param content
+     * @param urlStr
      * @return
      */
-    private ArrayList<String> getUrlFromContent(String content) {
+    private ArrayList<String> getUrlFromContent(String content, String urlStr) {
+//        System.out.println(content);
+        //处理带有query的url，如：http://image.baidu.com/?fr=bk
+        int index;
+        if ((index = urlStr.lastIndexOf("?")) != -1) {
+            urlStr = urlStr.substring(0, index);
+        }
+        if (!urlStr.endsWith("/")) {
+            urlStr += "/";
+        }
         //.*?非贪婪模式
         Pattern pattern = Pattern.compile("<a\\s+(href=\".*?\")>", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(content);
@@ -56,8 +65,22 @@ public class Gather {
             int start = url.indexOf("\"");
             int end = url.indexOf("\"", start + 1);
             String rs = url.substring(start+1, end);
+            //处理相对地址
+            if (!(rs.startsWith("http://") || rs.startsWith("https://"))) {
+                if (rs.startsWith("#")) {
+                    continue;
+                }
+                if (rs.startsWith("//")) {
+                    //处理"//"开头的地址
+                    rs = urlStr.substring(0, urlStr.indexOf(":")) + rs;
+                } else if (rs.startsWith("/")) {
+                    rs = urlStr + rs.substring(1);
+                } else {
+                    rs = urlStr + rs;
+                }
+                urls.add(rs);
+            }
             System.out.println(rs);
-            urls.add(rs);
         }
         return urls;
     }
--- src/com/yliec/Main.java
+++ src/com/yliec/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            new Gather().getPage("http://www.douban.com");
+            new Gather().getPage("http://www.ibm.com/developerworks/cn/java/j-lo-dyse1/");
         } catch (MalformedURLException e) {
             e.printStackTrace();
             System.out.println("错误的url");
